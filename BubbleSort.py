a39edeceab7b550943b3639887ab6e8c0aac5ca6
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2020-12-01 17:21:24 +0530

    Add files via upload

---
+++ BubbleSort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+
+    for n in range(len(arr)-1, 0, -1):
+        for i in range(n):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1]=temp
+
+    return arr
